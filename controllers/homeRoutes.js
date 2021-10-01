38e95ba54aa4fa770ac301b78b7d885222ee141f
Amanda Morgan <AmandaC0022@gmail.com>
2021-10-01 11:47:44 -0500

    added nodemon as a dependency

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,29 +1,23 @@
 const router = require('express').Router();
-// const { Project, User } = require('../models');
+const { Hashtag } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    // const projectData = await Project.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ['name'],
-    //     },
-    //   ],
-    // });
+    const allHashtagData = await Hashtag.findAll();
 
     // Serialize data so the template can read it
-    // const projects = projectData.map((project) => project.get({ plain: true }));
+    const hashtags = allHashtagData.map((hashtag) => hashtag.get({ plain: true }));
 
+    res.status(200).json(hashtags);
     // Pass serialized data and session flag into template
-    res.render('homepage'
+    // res.render('homepage'
     // , { 
-    //   projects, 
-    //   logged_in: req.session.logged_in 
+    //   hashtags, 
+    //   // logged_in: req.session.logged_in 
     // }
-    );
+    // );
   } catch (err) {
     res.status(500).json(err);
   }
