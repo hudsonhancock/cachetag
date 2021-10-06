ef41e53c74f5eb5d7e2393122ceb5f5967981534
Amanda Morgan <AmandaC0022@gmail.com>
2021-10-05 20:29:02 -0500

    hardcoded user's hashtag page with save button dropdown

--- controllers/views/profileHashtagsRoutes.js
+++ controllers/views/profileHashtagsRoutes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 //displays the User's Hashtags Page
 router.get('/', async (req, res) => {
     try {
-      res.render('profile_hashtags'); 
+      res.render('savedHashtags'); 
     } catch (err) {
       res.status(500).json(err);
     }
