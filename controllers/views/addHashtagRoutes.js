5f2036f439ad449045719c5a0e067d98d52710e3
Hungry Man <katsu.matsushita@gmail.com>
2021-10-08 00:55:38 -0400

    Search for hashtags working The search for hashtags now shows results. The addHashtag.handlebar now shows results and shows niches to save. The save functionality is still not working.

--- controllers/views/addHashtagRoutes.js
+++ controllers/views/addHashtagRoutes.js
@@ -2,6 +2,36 @@ const router = require("express").Router();
 const rp = require("request-promise");
 const cheerio = require("cheerio");
 const request = require("request");
+const { User, Niche, Collection } = require("../../models");
+
+// request has to be in its own promisifed wrapper, so that it can be forced to wait before the rest of the code executes
+// solution found here: https://stackoverflow.com/questions/38428027/why-await-is-not-working-for-node-request-module
+function doRequest(urlKey) {
+	return new Promise( (resolve, reject) => {
+		let newTags = [];
+		request(
+			{
+				method: "GET",
+				url: urlKey,
+			},
+			(err, res, body) => {
+				if (err) return reject(err);
+				let $ = cheerio.load(body);
+				let h1El = $(".tag");
+				let wordsArray = h1El.text().split("#");
+				let noEmpties = wordsArray.filter((e) => e);
+				noEmpties.forEach((tag) => {
+					if (tag.charAt(tag.length-1) == "X") {
+						newTags.push(tag.slice(0, -1));
+					} else { newTags.push(tag)};
+				});
+				console.log("from inside the request" + newTags);
+				resolve(newTags)
+			}
+		);
+	});
+};
+
 //displays the addHashtag Page
 router.get("/", async (req, res) => {
 	try {
@@ -14,30 +44,32 @@ router.get("/", async (req, res) => {
 router.get("/:keyword", async (req, res) => {
 	try {
 		let keyword = req.params.keyword;
+		const URL = `https://www.tagsfinder.com/en-us/ajax/?hashtag=${keyword}&limit=10&country=us&custom=&type=live`;
+		console.log(URL);
 
-		await request(
-			{
-				method: "GET",
-				url: `https://www.tagsfinder.com/en-us/ajax/?hashtag=${keyword}&limit=10&country=us&custom=&type=live`,
+		let hashtags = await doRequest(URL);
+		
+		const nicheData = await User.findAll( {
+			where: { user_id: req.session.user_id },
+			attributes: {
+			  exclude: ["password"],
 			},
-			(err, res, body) => {
-				if (err) return console.error(err);
-				let $ = cheerio.load(body);
-				let h1El = $(".tag");
-				let wordsArray = h1El.text().split("#");
-				let noEmpties = wordsArray.filter((e) => e);
-				let newTag = [];
-				noEmpties.forEach((tag) => {
-					if (tag.charAt(tag.length-1) == "X") {
-						newTag.push(tag.slice(0, -1));
-					} else { newTag.push(tag)};
-				});
-				
-				//relatedHashtags = noEmpties = noEmpties.map((i) => "#" + i);
-				console.log(newTag);
-			}
-		);
-		res.status(200).json(newTag);
+			include: [{ model: Niche, through: {attributes: []}, }]
+		  });
+		// serialize the nicheData so that we can work on it
+		const xniche = nicheData.map((nicheCol) => nicheCol.get({ plain: true }));
+		//console.log("This is xNiche after serialization: " + JSON.stringify(xniche, null, 2));
+	
+		const yniche = xniche[0];
+		const niches = yniche.niches;
+		//console.log("This is yniche: " + JSON.stringify(yniche, null, 2));
+		console.log(niches);
+		console.log(hashtags);
+		// hashtags = ["test1", "test2", "test3"];
+		res.render("addHashtag", {
+			hashtags,
+			niches,
+		});
 	} catch (err) {
 		res.status(400).json(err);
 	}
--- public/js/add-hashtags.js
+++ public/js/add-hashtags.js
@@ -1,33 +1,69 @@
-  var buttons = document.querySelectorAll(".dropdown-content"); 
-  var savebtns = document.querySelectorAll(".saveButtonDiv");  
-  var delbtns = document.querySelectorAll(".delete_hashtag");
-  var searchBtn = document.querySelector("#searchBtn");
+var buttons = document.querySelectorAll(".dropdown-content"); 
+var savebtns = document.querySelectorAll(".saveButtonDiv");  
+var delbtns = document.querySelectorAll(".delete_hashtag");
+var searchBtn = document.querySelector("#searchBtn");
   
-  let niche;
-  let hashtag;
-  let hashtagId;
+let niche;
+let hashtag;
+let hashtagId;
 
-  var getHashtag = (event) => {
-      hashtag = event.target.dataset.hashtag;
-        hashtagId = event.target.dataset.hashtag_id;
-      console.log("This is the hashtag: " + event.target.dataset.hashtag + "\n This is the hashtag_id: " + hashtagId);
-  }; 
+var getHashtag = (event) => {
+  hashtag = event.target.dataset.hashtag;
+  hashtagId = event.target.dataset.hashtag_id;
+  console.log("This is the hashtag: " + event.target.dataset.hashtag + "\n This is the hashtag_id: " + hashtagId);
+}; 
   
   
-  // + Niche Selection Handler - This is what sends the POST requests to the three routes needed to add hashtag to a user's selected niche
-  var chooseNiche = async (event) => {
-    // + This is the code that sets the selected niche to 'niche'
-    event.stopPropagation(); 
-    niche = event.target.textContent;
-    niche_id = event.target.id;
-    // console.log("This is the niche name: " + niche + "\nThis is the hashtag: " + hashtag + ":" + hashtagId + "\n " + niche_id)
-    console.log(niche_id);
+// + Niche Selection Handler - This is what sends the POST requests to the three routes needed to add hashtag to a user's selected niche
+var chooseNiche = async (event) => {
+  // + This is the code that sets the selected niche to 'niche'
+  event.stopPropagation(); 
+  niche = event.target.textContent;
+  niche_id = event.target.id;
+  // console.log("This is the niche name: " + niche + "\nThis is the hashtag: " + hashtag + ":" + hashtagId + "\n " + niche_id)
+  //console.log(niche_id);
 
-    // + Fetch POST request to /api/hashtags/ endpoint with hashtag text to create Hashtag and returns the new hashtag object that was created and data about the niche that the user chose.
-    if (niche) {
-        const response = await fetch(`/api/hashtags`, {
+  // + Fetch POST request to /api/hashtags/ endpoint with hashtag text to create Hashtag and returns the new hashtag object that was created and data about the niche that the user chose.
+  if (niche) {
+    const response = await fetch(`/api/hashtags`, {
+      method: 'POST',
+      body: JSON.stringify({ hashtag, niche_id }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(jsonData => {
+      // + We send the hashtag_id and the niche_id to the /api/user_hashtag endpoint
+      console.log("NEW HASHTAG:")
+      console.log(jsonData.newHashtag);
+      console.log("NICHE DATA FROM SERVER:")
+      console.log(jsonData.nicheData);
+
+      let hashtag_id = jsonData.newHashtag.hashtag_id;
+      let niche_id = jsonData.nicheData.niche_id;
+      let collection_id = jsonData.nicheData.collection_id;
+
+      const response = fetch(`/api/user_hashtag`, {
+        method: 'POST',
+        body: JSON.stringify({ niche_id, hashtag_id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        console.log("RESPONSE FROM /API/USER_HASHTAG:")
+        console.log(jsonData);
+
+        // + Here we send a post to /api/collectiontags/ endpoint with the hashtag_id and collection_id 
+        const response = fetch(`/api/collectiontags`, {
           method: 'POST',
-          body: JSON.stringify({ hashtag, niche_id }),
+          body: JSON.stringify({ collection_id, hashtag_id }),
           headers: {
             'Content-Type': 'application/json',
           },
@@ -36,65 +72,27 @@
           return response.json();
         })
         .then(jsonData => {
-          // + We send the hashtag_id and the niche_id to the /api/user_hashtag endpoint
-          console.log("NEW HASHTAG:")
-          console.log(jsonData.newHashtag);
-          console.log("NICHE DATA FROM SERVER:")
-          console.log(jsonData.nicheData);
-
-          let hashtag_id = jsonData.newHashtag.hashtag_id;
-          let niche_id = jsonData.nicheData.niche_id;
-          let collection_id = jsonData.nicheData.collection_id;
-
-          const response = fetch(`/api/user_hashtag`, {
-            method: 'POST',
-            body: JSON.stringify({ niche_id, hashtag_id }),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          })
-          .then(response => {
-            return response.json();
-          })
-          .then(jsonData => {
-            console.log("RESPONSE FROM /API/USER_HASHTAG:")
-            console.log(jsonData);
-
-            // + Here we send a post to /api/collectiontags/ endpoint with the hashtag_id and collection_id 
-            const response = fetch(`/api/collectiontags`, {
-              method: 'POST',
-              body: JSON.stringify({ collection_id, hashtag_id }),
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            })
-            .then(response => {
-              return response.json();
-            })
-            .then(jsonData => {
-              console.log(jsonData);
-            })
-
-          })
+          console.log(jsonData);
         })
+      })
+    })
         
-        if (response.ok) {
-          document.location.replace('/addHashtag');
-          console.log("Hashtag Created: " + JSON.parse(response.json()));
-        } else {
-          alert('Failed to create niche');
-          console.log(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace('/addHashtag');
+      console.log("Hashtag Created: " + JSON.parse(response.json()));
+    } else {
+        alert('Failed to create niche');
+        console.log(response.statusText);
       }
-
-      // + Fetch POST request to /api/user_hastag/ with hashtag_id and niche_id to create HashtagNiche and returns the hashtag_id 
-  }; 
+  }
+// + Fetch POST request to /api/user_hastag/ with hashtag_id and niche_id to create HashtagNiche and returns the hashtag_id 
+}; 
     
 const delButtonHandler = async (event) => {
-event.stopPropagation(); 
-event.preventDefault(); 
+  event.stopPropagation(); 
+  event.preventDefault(); 
 
-if (event.target.hasAttribute('data-id')) {
+  if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
     const nicheID = event.target.getAttribute("data-nicheID");
     console.log(nicheID);
@@ -111,16 +109,17 @@ if (event.target.hasAttribute('data-id')) {
     } else {
       alert('Failed to delete hashtag');
     };
-};
+  };
 };
 
 var searchHashTag = async (event) => {
   event.stopPropagation();
   event.preventDefault();
   let searchText = document.querySelector("#searchInput");
-  const hashTags = await fetch(`/addHashtag/${searchText.value}`, {
-    method: 'GET',
-  });
+  /* const searchHash = await fetch(`/addHashtag/${searchText.value}`, {
+    method: "GET"
+  }); */
+  document.location.replace(`/addHashtag/${searchText.value}`);
 }; 
 
 buttons.forEach(function(button) {
